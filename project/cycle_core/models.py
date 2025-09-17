06f3b1095212143183d64fe41d2879593383c3b6
Judd Russel <juddrussel.kalalo@gmail.com>
2025-09-17 16:27:19 +0200

    added app: cycle_core

---
+++ project/cycle_core/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+# Create your models here.
+class CycleDetails(models.Model):
+
+    CYCLE_DURATION_CHOICES = [(f'{i} DAYS', i) for i in range(22, 45)]
+    MENSTRUATION_DURATION_CHOICES = [(f'{i} DAY', i) if i == 1 else (f'{i} DAYS', i) for i in range(1, 11)]
+
+    last_menstruation_date = models.DateField()
+
+    avg_cycle_duration = models.IntegerField(
+        choices=CYCLE_DURATION_CHOICES,
+        blank=False,
+        null=False,
+    )
+
+    avg_menstruation_duration = models.IntegerField(
+        choices=MENSTRUATION_DURATION_CHOICES,
+        blank=False,
+        null=False,
+    )
---
+++ project/cycle_core/tests/test_models.py
@@ -0,0 +1,7 @@
+from django.test import TestCase
+
+class CycleDetailsTestCase(TestCase):
+    def __init__(self, methodName = "runTest"):
+        
+
+    def test_successful_insert(self):
