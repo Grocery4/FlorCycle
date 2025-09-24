256df32cf9708ef8d0bf65fa8fa9cc3db32d1c2b
Judd Russel <juddrussel.kalalo@gmail.com>
2025-09-24 15:23:18 +0200

    added if conditions to check whether predictions, calendar and CycleDetails data has been submitted, removed default guest_mode.tests file, implemented creation of prediction and calendars to be passed onto template in guest_mode.views

--- project/guest_mode/views.py
+++ project/guest_mode/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 
 from cycle_core.forms import CycleDetailsForm
 from cycle_core.services import PredictionBuilder
+from .services import generateCalendars
 
 # Create your views here.
 def show_form(request):
@@ -13,12 +14,15 @@ def show_form(request):
         if form.is_valid():
             
             cd = form.save(commit=False)
+            predictions = PredictionBuilder.generateMultiplePredictions(cd, 3)
 
             ctx['last_menstruation_date'] = cd.last_menstruation_date
             ctx['avg_cycle_duration'] = cd.avg_cycle_duration
             ctx['avg_menstruation_duration'] = cd.avg_menstruation_duration
-
-            ctx['predictions'] = PredictionBuilder.generateMultiplePredictions(cd, 3)
+            ctx['predictions'] = predictions
+            
+            # TODO render calendars
+            ctx['calendars'] = generateCalendars(predictions)
 
             return render(request, 'show_form.html', ctx)
 
