9a71d58d16d359debbf5b23bdbae25ea9c53b00d
Judd Russel <juddrussel.kalalo@gmail.com>
2025-10-01 15:59:20 +0200

    added validator for cv, now accepting pdfs and docxs only, added clean_license_number() method to display error whenever license_number has a duplicate, form now displays errors

--- project/users/forms.py
+++ project/users/forms.py
@@ -1,17 +1,25 @@
 from django.contrib.auth.forms import UserCreationForm
 from django import forms
+from django.core.validators import FileExtensionValidator
 from .models import CustomUser, ModeratorProfile, DoctorProfile, PartnerProfile, PremiumProfile
 
 class ModeratorSignupForm(UserCreationForm):
     pass
 
 class DoctorSignupForm(UserCreationForm):
-    cv = forms.FileField(required=True)
+    cv = forms.FileField(validators=[FileExtensionValidator(allowed_extensions=["pdf", "docx"])], required=True)
     license_number = forms.CharField(max_length=100)
 
     class Meta(UserCreationForm.Meta):
         model = CustomUser
         fields = ('username', 'email')
+
+    def clean_license_number(self):
+        license_number = self.cleaned_data.get("license_number")
+        if DoctorProfile.objects.filter(license_number=license_number).exists():
+            raise forms.ValidationError("This license number is already registered.")
+        return license_number
+
 
     def save(self, commit=True):
         user = super().save(commit=False)
--- project/users/views.py
+++ project/users/views.py
@@ -2,18 +2,15 @@ from django.shortcuts import render
 
 from .forms import DoctorSignupForm
 
+
 # Create your views here.
-#TODO - create real view
-#TODO - validate only pdf files
-#TODO - TEST license_no / username / email uniqueness
-# TODO - display error
 def doctor_form(request):
     ctx = {}
     ctx['form'] = DoctorSignupForm()
-
     if request.method == 'POST':
         form = DoctorSignupForm(request.POST, request.FILES)
+        ctx['form'] = form
         if form.is_valid():
             form.save()
-
+    
     return render(request, 'test.html', ctx)
