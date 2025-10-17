ec1ce9db5c1fac9eb568af0e788c24abcfc53e32
Judd Russel <juddrussel.kalalo@gmail.com>
2025-10-17 10:00:25 +0200

    created templates/guest_mode to avoid ambiguity in imports

--- project/florcycle/urls.py
+++ project/florcycle/urls.py
@@ -25,4 +25,5 @@ urlpatterns = [
     path('guest-mode/', include('guest_mode.urls')),
     path("accounts/", include("django.contrib.auth.urls")),
     path('accounts/', include('users.urls')),
+    path('dashboard/', include('dashboard.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
