37982a5d80d6f2c0d38b2dc3d875e9a1db197a49
Judd Russel <juddrussel.kalalo@gmail.com>
2025-10-23 11:46:43 +0200

    implemented wrapper decorator to check for initial configuration

--- project/dashboard/services.py
+++ project/dashboard/services.py
@@ -12,3 +12,19 @@ def user_type_required(allowed_types, redirect_url='login'):
             return view_func(request, *args, **kwargs)
         return _wrapped_view
     return decorator
+
+def configured_required(view_func):
+    @wraps(view_func)
+    def _wrapped_view(request, *args, **kwargs):
+        user = request.user
+
+        if not user.is_authenticated:
+            return redirect('login')
+
+        profile = getattr(user, 'standardprofile', None) or getattr(user, 'premiumprofile', None)
+        if not profile or not profile.is_configured:
+            return redirect('dashboard:setup_page')
+
+        return view_func(request, *args, **kwargs)
+
+    return _wrapped_view
