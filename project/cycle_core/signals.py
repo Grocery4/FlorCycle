36bdf890005337aae4c235fa76620c578861893c
Judd Russel <juddrussel.kalalo@gmail.com>
2025-11-10 22:38:17 +0100

    refactored exit conditions in initCycleStatsOnCycleDetailsCreation. now log_count is initialized with actual number of non-prediction CycleWindows instances, implemented teardownCycleStatsOnCycleDetailsDeletion, implemented generateOrUpdatePredictions with guard to keep from creating predictions twice on CycleDetails and subsequent CycleStats initialization, implemented automatic updateLogCount(), implemented updateStatsOnLogChange

--- project/cycle_core/signals.py
+++ project/cycle_core/signals.py
@@ -1,24 +1,76 @@
 from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
 
-from .models import CycleDetails, CycleStats
+from .models import CycleDetails, CycleWindow, CycleStats
+from .services import generatePredictionBasedOnLogCount, updateCycleStats
 
-#TODO - test
+#TODO - test this mf
 @receiver(post_save, sender=CycleDetails)
 def initCycleStatsOnCycleDetailsCreation(sender, instance, created, **kwargs):
-	# Only act when a CycleDetails instance is created
-	if not created:
-		return
-
+	# Only act when a CycleDetails instance is created and is bound to a user
 	user = getattr(instance, 'user', None)
-	if user is None:
-		# Nothing to do if CycleDetails isn't bound to a user. Should never happen
+	if not created or user is None:
 		return
 
 	# Initialise defaults from CycleDetails but don't overwrite existing CycleStats
 	defaults = {
-		'avg_cycle_duration': float(getattr(instance, 'avg_cycle_duration', 28)),
-		'avg_menstruation_duration': float(getattr(instance, 'avg_menstruation_duration', 5)),
+		'avg_cycle_duration': int(getattr(instance, 'avg_cycle_duration', 28)),
+		'avg_menstruation_duration': int(getattr(instance, 'avg_menstruation_duration', 5)),
+		'log_count' : int(CycleWindow.objects.filter(user=user, is_prediction=False).count())
 	}
 
-	CycleStats.objects.get_or_create(user=user, defaults=defaults)
+	CycleStats.objects.update_or_create(user=user, defaults=defaults)
+
+
+# Clean up CycleStats (and predicted windows) when CycleDetails is deleted
+@receiver(post_delete, sender=CycleDetails)
+def teardownCycleStatsOnCycleDetailsDeletion(sender, instance, **kwargs):
+    user = getattr(instance, 'user', None)
+    if not user:
+        return
+
+    # remove stats so a fresh one can be created on next CycleDetails create
+    CycleStats.objects.filter(user=user).delete()
+    # remove any generated predictions for this user
+    CycleWindow.objects.filter(user=user, is_prediction=True).delete()
+
+
+
+#TODO - test this mf
+@receiver(post_save, sender=CycleDetails)
+@receiver(post_save, sender=CycleStats)
+def generateOrUpdatePredictions(sender, instance, created, **kwargs):
+	user = getattr(instance, 'user', None)
+
+	if sender is CycleDetails and created:
+		return
+
+	if user:
+		predictions = generatePredictionBasedOnLogCount(user)
+		CycleWindow.objects.filter(user=user, is_prediction=True).delete()
+		CycleWindow.objects.bulk_create(predictions)
+
+#TODO - test this mf
+@receiver([post_save, post_delete], sender=CycleWindow)
+def updateLogCount(sender, instance, **kwargs):
+    if instance.is_prediction:
+        return  # only count real logs
+
+    stats = CycleStats.objects.filter(user=instance.user).first()
+    if stats:
+        stats.log_count = CycleWindow.objects.filter(user=instance.user, is_prediction=False).count()
+        stats.save()
+
+#TODO - test this mf
+@receiver([post_save, post_delete], sender=CycleWindow)
+def updateStatsOnLogChange(sender, instance, **kwargs):
+	if instance.is_prediction:
+		return
+	if not instance.user:
+		return
+    
+	try:
+		stats = CycleStats.objects.get(user=instance.user)
+	except CycleStats.DoesNotExist:
+		return
+	updateCycleStats(stats)
