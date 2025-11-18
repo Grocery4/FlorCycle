3b719edb5f996c25c470df7f5e5be85dd3dc69d7
Judd Russel <juddrussel.kalalo@gmail.com>
2025-11-18 12:27:31 +0100

    implemented: Symptom, Mood, Medication classes, DailyLog, SymptomLog, MoodLog, IntercourseLog, MedicationLOg classes

--- project/log_core/models.py
+++ project/log_core/models.py
@@ -4,25 +4,27 @@ from django.conf import settings
 from cycle_core.models import CycleWindow
 
 # Create your models here.
+class Symptom(models.Model):
+    name = models.Charfield(max_length=50, unique=True)
+    category = models.CharField(max_length=50, blank=True, null=True)
+
+class Mood(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    category = models.CharField(max_length=50, blank=True, null=True)
+
+class Medication(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+
 class DailyLog(models.Model):
+    OVULATION_TEST_CHOICES = [('POSITIVE', 'Positive'), ('NEGATIVE', 'Negative')]
+    FLOW_CHOICES = [(0, 'Spotting'), (1, 'Light'), (2, 'Medium'), (3, 'Heavy')]
+
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     date = models.DateField()
 
-    OVULATION_TEST_CHOICES = [('POSITIVE', 'Positive'), ('NEGATIVE', 'Negative')]
-
-    # optional metadata for each day
-    note = models.TextField(max_length=500, blank=True)
-
-    # TODO - check whether to use separate subclass or choices
-    # mood = 
-    # symptoms = 
-    # intercourse = 
-    # medication = 
-
-    weight = models.FloatField(null=True, blank=True)
-    temperature = models.FloatField(null=True, blank=True)
-    ovulation_test = models.CharField(choices=OVULATION_TEST_CHOICES, blank=True)
-
+    # if the linked CycleWindow is deleted: it still exists as a standalone log for a given day
+    # if a CycleWindow is created in its proximity, it will be linked to it.
     cycle_window = models.ForeignKey(
         CycleWindow,
         null=True,
@@ -30,8 +32,49 @@ class DailyLog(models.Model):
         on_delete=models.SET_NULL
     )
 
+    # optional metadata for each day
+    note = models.TextField(max_length=500, blank=True, null=True)
+    flow = models.IntegerField(choices=FLOW_CHOICES, blank=True, null=True)
+    weight = models.FloatField(null=True, blank=True, null=True)
+    temperature = models.FloatField(null=True, blank=True, null=True)
+    # TODO - calculate ovulation date average and ovulation window average through ovulation_test=POSITIVE query \
+    # MAYBE IN ANALYTICS APP, not MVP
+    ovulation_test = models.CharField(choices=OVULATION_TEST_CHOICES, blank=True, null=True)
+
     class Meta:
         unique_together = ('user', 'date')  # one log per day
+        ordering = ['-date']
 
     def __str__(self):
         return f"{self.user} - {self.date}"
+
+class SymptomLog(models.Model):
+    INTENSITY_CHOICES = [(1, 'Low'), (2, 'Medium'), (3, 'High')]
+
+    log = models.ForeignKey(DailyLog, on_delete=models.CASCADE, related_name='symptoms')
+    symptom = models.ForeignKey(Symptom, on_delete=models.CASCADE)
+    intensity = models.IntegerField(choices=INTENSITY_CHOICES, null=True, blank=True)
+
+    class Meta:
+        unique_together = ('log', 'symptom')
+
+class MoodLog(models.Model):
+    log = models.ForeignKey(DailyLog, on_delete=models.CASCADE, related_name='moods')
+    mood = models.ForeignKey(Mood, on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = ('log', 'mood')
+
+
+class IntercourseLog(models.Model):
+    log = models.OneToOneField(DailyLog, on_delete=models.CASCADE, related_name='intercourse')
+    protected = models.BooleanField(blank=True, null=True)
+    orgasm = models.BooleanField(blank=True, null=True)
+    quantity = models.PositiveSmallIntegerField()
+
+class MedicationLog(models.Model):
+    log = models.ForeignKey(DailyLog, on_delete=models.CASCADE, related_name='medication')
+    medication = models.ForeignKey(Medication, on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = ('log', 'medication')
