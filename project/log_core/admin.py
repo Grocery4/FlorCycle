a91306523c289173ec2fea830d84e82d10f6dc59
Judd Russel <juddrussel.kalalo@gmail.com>
2025-11-21 11:43:14 +0100

    temporarily added Logs models to admin to be able to quickly create objects and test

--- project/log_core/admin.py
+++ project/log_core/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from .models import DailyLog, SymptomLog, MoodLog, IntercourseLog, MedicationLog
+
 # Register your models here.
+admin.site.register(DailyLog)
+admin.site.register(SymptomLog)
+admin.site.register(MoodLog)
+admin.site.register(IntercourseLog)
+admin.site.register(MedicationLog)
