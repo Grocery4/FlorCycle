f8d7e344b7d07f6de353606e3dbc20d02c210278
Judd Russel <juddrussel.kalalo@gmail.com>
2025-12-10 13:06:01 +0100

    added homepage_readonly url route

--- project/dashboard/urls.py
+++ project/dashboard/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     path('setup/', views.setup, name='setup_page'),
     path('settings/', views.settings, name='settings_page'),
     path('setup/partners', views.partner_setup, name='partner_setup_page'),
+    path('partner-mode/', views.homepage_readonly, name='homepage_readonly'),
     re_path(r'^(?:home/)?$', views.homepage, name='homepage'),
     path('log-period/', views.add_period, name='add_period'),
     path('logs/', views.cycle_logs, name='logs_page'),
