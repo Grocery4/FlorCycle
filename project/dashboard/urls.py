28ff054f90992a6711f5e0d812675f9de8aff54a
Judd Russel <juddrussel.kalalo@gmail.com>
2025-12-16 18:07:04 +0100

    added calendar view route

--- project/dashboard/urls.py
+++ project/dashboard/urls.py
@@ -16,6 +16,7 @@ urlpatterns = [
     path('logs/', views.cycle_logs, name='logs_page'),
     path('add-log/', views.add_log, name='add_log'),
     path('stats/', views.stats, name='stats'),
+    path('calendar/', views.calendar_view, name='calendar_view'),
     path('ajax/load-log/', views.ajax_load_log, name='ajax_load_log'),
     path('ajax/navigate-calendar/', views.ajax_navigate_calendar, name='ajax_navigate_calendar'),
     path('ajax/load-stats', views.ajax_load_stats, name='ajax_load_stats')
