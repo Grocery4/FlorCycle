ef4b18a5f51f82e954e328969660a73370c000f7
Judd Russel <juddrussel.kalalo@gmail.com>
2026-01-10 11:23:28 +0100

    added localhost to allowed_hosts

--- project/florcycle/settings.py
+++ project/florcycle/settings.py
@@ -27,8 +27,8 @@ SECRET_KEY = 'django-insecure-h-p_sev3on@5kadl8*f0ne18$6ye#=2xiwd9rr=79l8)!1i#%e
 DEBUG = True
 
 # Testing over internet purposes
-ALLOWED_HOSTS = ['127.0.0.1', 'dietpi.tail4aea60.ts.net']
-CSRF_TRUSTED_ORIGINS = ['http://127.0.0.1:8000', 'https://dietpi.tail4aea60.ts.net:8000']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'dietpi.tail4aea60.ts.net']
+CSRF_TRUSTED_ORIGINS = ['http://localhost:8000', 'http://127.0.0.1:8000', 'https://dietpi.tail4aea60.ts.net:8000']
 
 # Application definition
 
